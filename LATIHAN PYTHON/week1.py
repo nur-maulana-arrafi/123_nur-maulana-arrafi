71d3459aff74bcc19294906c3affb7cdf5fe5e85
nur-maulana-arrafi <143861052+nur-maulana-arrafi@users.noreply.github.com>
2023-09-02 11:17:08 +0700

    Add: Tugas
    Add: Tugas

---
+++ LATIHAN PYTHON/week1.py
@@ -0,0 +1,27 @@
+# udah belajar tentang variabel dan tipe data
+a = 5
+b = 3.6
+c = "ayam"
+d = (87,29,23)
+e = {"ayam",2000,5}
+f = [23,12,15]
+
+print("ini adalah tipe data integer", a)
+
+# belajar operator aritmatika
+
+# menghitung luas persegi
+# Ini Statis
+sisi1 = 5
+sisi2 = 7
+hasil = sisi1*sisi2
+# Ini Dinamis
+angka = int(input("Masukkan Angka 1 : "))
+angka2 = int(input("Masukkan Angka 2 : "))
+sisi1 = angka
+sisi2 = angka2
+hasil = sisi1*sisi2
+
+print("luas persegi ini adalah ", sisi1*sisi2)
+
+
